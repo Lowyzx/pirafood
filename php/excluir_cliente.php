6827ba7e3de3817a12ddd051c2eb6ffc64d812ef
Gabriel <gabrielsasukeclash@gmail.com>
2025-09-21 19:41:09 -0300

    alterações do dia 21/09

--- php/excluir_cliente.php
+++ php/excluir_cliente.php
@@ -2,7 +2,7 @@
 include 'connection.php';
 $id = $_GET['id'];
 
-$sql->query("DELETE FROM clientes WHERE id = $id");
+$sql->query("DELETE FROM cliente WHERE id = $id");
 header("Location: lista_cliente.php");
 exit();
 ?>
